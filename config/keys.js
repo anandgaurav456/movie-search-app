c504a5e146dec50e378ab95cb9a3bf9ba88f0bcb
Anand Gaurav <anandgaurav456@gmail.com>
2021-07-07 03:39:29 +0530

    Connected MongoDB Atlas

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    MongoURI: "mongodb://localhost:27017/login"
-}
+  // MongoURI: "mongodb://localhost:27017/login"
+  MongoURI:
+    "mongodb+srv://admin:ov6qO1iltciVdZBD@cluster0.rwn3i.mongodb.net/imdb?retryWrites=true&w=majority",
+};
